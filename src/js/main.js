1284415acec8358e0935a0aa4c8debb59943f7b3
Marta <polanskamarta.com>
2025-05-06 14:07:57 +0200

    aboutpage fixed (responsive)

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@
 
 const menuIcon = document.querySelector('.menu-icon');
 const cnt = document.querySelector('.container');
+const loader = document.querySelector('.loader-wrapper');
 
 const navItems = document.querySelectorAll('.nav-items > a');
 const path = window.location.pathname.split('/').pop();
@@ -10,7 +11,7 @@ const CONTACT_HREF = '#contact';
 function navItemClickHandler(e) {
 	if (e.target.getAttribute('href') === CONTACT_HREF) {
 		cnt.classList.remove('navigate');
-		closedMenu();
+		unblockedBody();
 	}
 }
 
@@ -36,11 +37,10 @@ navItems.forEach((item) => {
 
 menuIcon.addEventListener('click', (e) => {
 	cnt.classList.toggle('navigate');
-
 	if (cnt.classList.contains('navigate')) {
-		mobileScrollBlockageTO = setTimeout(openedMenu, 300);
+		blockedBody();
 	} else {
-		closedMenu();
+		unblockedBody();
 	}
 });
 
@@ -52,21 +52,45 @@ const ROOT = document.querySelector(':root');
 const BODY = document.body;
 let mobileScrollBlockageTO;
 let scrollPosition = 0;
+let scrollBarWidth = 0;
 
-function openedMenu() {
-	scrollPosition = window.scrollY;
+function blockedBody() {
+	//scrollPosition = window.scrollY; //if body == absolute
+	scrollY = parseInt(BODY.style.top || '0', 10) * -1;
+	scrollBarWidth = window.innerWidth - document.documentElement.clientWidth;
+
 	ROOT.style.scrollBehavior = 'auto';
 	BODY.style.overflow = 'hidden';
-	BODY.style.position = 'absolute';
+	BODY.style.position = 'fixed';
 	BODY.style.top = `-${scrollPosition}px`;
 	BODY.style.width = '100%';
+	BODY.style.paddingRight = `${scrollBarWidth}px`;
 }
 
-function closedMenu(backToPreviousScrollPosition = true) {
+function unblockedBody(backToPreviousScrollPosition = true) {
 	clearTimeout(mobileScrollBlockageTO);
 	BODY.style.overflow = 'visible';
 	BODY.style.position = 'static';
 	BODY.style.top = '0px';
+	BODY.style.paddingRight = '0px';
 	if (backToPreviousScrollPosition) window.scrollTo(0, scrollPosition);
 	ROOT.style.scrollBehavior = 'smooth';
 }
+
+//--------------| loader |----------------
+
+window.addEventListener('load', () => {
+	if (cnt.classList.contains('index-container')) {
+		console.log('!!!!!!!!!!!!');
+
+		blockedBody();
+
+		setTimeout(() => {
+			loader.classList.add('hide');
+		}, 3000);
+		setTimeout(() => {
+			cnt.classList.add('show');
+			unblockedBody();
+		}, 2500);
+	}
+});
